c04126de12b63d9e7cb37f0ea78a660c331e808c
ylt94 <857425891@qq.com>
2019-03-13 15:07:34 +0800

    update

--- server/tcp/server.php
+++ server/tcp/server.php
@@ -10,6 +10,9 @@ $server->set(
     [
         'reactor_num' => 1,
         'worker_num' => 2,
+        'task_work_num' => 2,
+        'task_ip_mode' => 2,
+        'message_queue_key' => ftok(__DIR__,1),
         'heartbeat_check_interval' => 3,//心跳检测间隔时间
         'heartbeat_idle_time' => 7,//tcp允许最大闲置时间
         'open_length_check'=>1,
@@ -30,19 +33,25 @@ $server->on('connect',function(swoole_server $server,int $fd){
 //注册消息接受监听事件
 $server->on('receive',function(swoole_server $server,int $fd,int $reactor_id, string $data){
     $send_data = $data;
-    $data = substr($data,4);
-    $data = json_decode($data,true);
-    echo $data['msg'].PHP_EOL;
-    echo '开始向客户端发送应答'.PHP_EOL;
-    $server->send($fd,$send_data);
+    // $data = substr($data,4);
+    // $data = json_decode($data,true);
+    // echo $data['msg'].PHP_EOL;
+    // echo '开始向客户端发送应答'.PHP_EOL;
+    // $server->send($fd,$send_data);
 
-    //广播
-    echo '开始广播udp'.PHP_EOL;
-    $udp_client = new Swoole\Client(SWOOLE_SOCK_UDP);
-    $udp_client->sendto('127.0.0.1',9801,'我是客户端');
-    echo '广播发送完毕'.PHP_EOL;
+    // //广播
+    // echo '开始广播udp'.PHP_EOL;
+    // $udp_client = new Swoole\Client(SWOOLE_SOCK_UDP);
+    // $udp_client->sendto('127.0.0.1',9801,'我是客户端');
+    // echo '广播发送完毕'.PHP_EOL;
     //$res = $udp_client->recv(1024 * 1024 * 2,1);
     //echo '接收到新消息，长度：'.strlen($data).PHP_EOL;
+    $server->task($data,2);
+});
+
+$server->on('task',function(swoole_server $server, int $task_id, int $src_worker_id, mixed $data){
+    echo 'task进程接收到任务,task_id:'.$task_id.'src_worker_id:'.$src_worker_id.PHP_EOL;
+    
 });
 
 //注册连接关闭监听事件
