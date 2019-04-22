2fddef287d51f1ab5d5113332d036ad56060cd29
ylt94 <857425891@qq.com>
2019-04-22 15:18:44 +0800

    1

--- process/process_pool.php
+++ process/process_pool.php
@@ -4,12 +4,17 @@ use Swoole\Process\Pool;
 use Swoole\Process;
 
 Process::daemon();
-$pool = new Pool(10);
+$pool = new Pool(1);
 
 $pool->on('WorkerStart',function($pool,$workerId){
     //echo var_dump($pool);
     //echo 'workerid:'.$workerId.PHP_EOL;
     sleep(20);
 });
+
+$pool->on("WorkerStop", function ($pool, $workerId) {
+    echo "Worker#{$workerId} is stopped\n".PHP_EOL;
+});
+
 
 $pool->start();
