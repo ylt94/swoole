e082e393a16e490b33a0baa35cc93295949295fe
ylt94 <857425891@qq.com>
2019-04-22 15:06:43 +0800

    1

---
+++ process/process_pool.php
@@ -0,0 +1,13 @@
+<?php
+
+use Swoole\Process\Pool;
+
+$pool = new Pool(10);
+
+$pool->on('WorkerStart',function($pool,$workerId){
+    echo var_dump($pool);
+    echo 'workerid:'.$workerId.PHP_EOL;
+    seelp(20);
+});
+
+$pool->start();
