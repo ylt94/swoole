2f9143494004c7d42fd183e9d671f113502f3644
ylt94 <857425891@qq.com>
2019-04-23 09:33:12 +0800

    1

--- process/process.php
+++ process/process.php
@@ -1,3 +1,18 @@
 <?php
 
 use Swoole\Process;
+
+$process1 = new Process(function(swoole_process $process1){
+    echo '进程1'.PHP_EOL;
+});
+$process2 = new Process(function(swoole_process $process2){
+    echo '进程2'.PHP_EOL;
+});
+$process3 = new Process(function(swoole_process $process3){
+    echo '进程3'.PHP_EOL;
+});
+
+$process1->start();
+$process2->start();
+$process3->start();
+Process::wait();
