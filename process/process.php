28b86d31b6cf7628f122222953a389018cfcdb46
ylt94 <857425891@qq.com>
2019-04-23 10:06:53 +0800

    1

--- process/process.php
+++ process/process.php
@@ -1,9 +1,17 @@
 <?php
 
 use Swoole\Process;
+use Swoole\Process\Pool;
 
 $process1 = new Process(function(swoole_process $process1){
     echo '进程1'.PHP_EOL;
+    $pool = new Pool(1);
+    $pool->on('WorkerStart',function($pool,$workerId){
+        //echo var_dump($pool);
+        echo 'workerid:'.$workerId.PHP_EOL;
+        sleep(20);
+    });
+    $pool->start();
 });
 $process2 = new Process(function(swoole_process $process2){
     echo '进程2'.PHP_EOL;
