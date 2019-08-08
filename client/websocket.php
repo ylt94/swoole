13319ea9330e17ee24f85a1a22e2765b2f3e6147
ylt94 <857425891@qq.com>
2019-08-08 18:09:03 +0800

    update

--- client/websocket.php
+++ client/websocket.php
@@ -1,6 +1,6 @@
 <?php
 
-$cli = new swoole_http_client('127.0.0.1', 9500);
+$cli = new \swoole_http_client('127.0.0.1', 9500);
 $cli->setHeaders([
     'Host' => $domainName,
     'UserAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
