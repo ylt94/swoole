b968c11e8a160e2a9ce7d53589a19b3757ebd123
ylt94 <857425891@qq.com>
2019-08-08 18:16:59 +0800

    update

--- client/websocket.php
+++ client/websocket.php
@@ -1,18 +1,21 @@
 <?php
 
-$cli = new \swoole_http_client('127.0.0.1', 9500);
-$cli->setHeaders([
-    'Host' => $domainName,
-    'UserAgent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-    'Accept' => 'text/html,application/xhtml+xml,application/xml',
-    'Accept-Encoding' => 'gzip',
-]);
-//异步回调客户端
-$client->on("receive", function(swoole_client $cli, $data){
-    echo '已获取到服务端返回数据：'.$data;
-   //$cli->send(str_repeat('A', 100)."\n");
-
-});
-$cli->upgrade('/', function ($cli) {
-    $cli->push("hello world");
+go(function () {
+    $server = new Co\Http\Server("127.0.0.1", 9500, false);
+    $server->handle('/', function ($request, $ws) {
+        $ws->upgrade();
+        while (true) {
+            $frame = $ws->recv();
+            if ($frame === false) {
+                echo "error : " . swoole_last_error() . "\n";
+                break;
+            } else if ($frame == '') {
+                break;
+            } else {
+                $ws->push("Hello {$frame->data}!");
+                $ws->push("How are you, {$frame->data}?");
+            }
+        }
+    });
+    $server->start();
 });
