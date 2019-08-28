9f770ee54d5eb1b62a2d819b0333b8d4aacbbccf
ylt94 <857425891@qq.com>
2019-08-28 20:06:57 +0800

    update

--- process/process.php
+++ process/process.php
@@ -14,6 +14,22 @@ class Test{
             self::$process[$i] = $process->pid;          
         }
         var_dump(self::$process);
+        while($kill_msg =  \swoole_process::wait(false)) {
+            echo '子进程被杀掉，信息：'.var_export($kill_msg,true);
+            $process = self::$process;
+            foreach($process as $i => $pid){
+                if($pid != $kill_msg['pid']){
+                    continue;
+                 }
+                 self::$process[$i] = 0;
+                 //如果不是kill -9信号，将会重新拉起被杀掉的进程
+                 if($kill_msg['signal'] != SIGKILL){
+                    $new_process = $this->createMarketProcess($i);
+                    self::$process[$i] = $new_process->pid;
+                 }
+                 break;
+            }
+        }
     }
 
     public function createProcess($i){
