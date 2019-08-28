d34c6b24638fa84987f1503f9f1902db2d064ea1
ylt94 <857425891@qq.com>
2019-08-28 19:44:08 +0800

    update

--- process/process.php
+++ process/process.php
@@ -3,24 +3,29 @@
 use Swoole\Process;
 use Swoole\Process\Pool;
 
-$process1 = new Process(function(swoole_process $process1){
-    echo '进程1'.PHP_EOL;
-    $pool = new Pool(1);
-    $pool->on('WorkerStart',function($pool,$workerId){
-        //echo var_dump($pool);
-        echo 'workerid:'.$workerId.PHP_EOL;
-        sleep(20);
-    });
-    $pool->start();
-});
-$process2 = new Process(function(swoole_process $process2){
-    echo '进程2'.PHP_EOL;
-});
-$process3 = new Process(function(swoole_process $process3){
-    echo '进程3'.PHP_EOL;
-});
+class Test{
 
-$process1->start();
-$process2->start();
-$process3->start();
-Process::wait();
+
+    protected static $process = [];
+
+    public function index(){
+        for($i = 0; $i < 3; $i++){
+            $process = $this->createProcess($i);
+            self::$process[$i] = $process->pid;          
+        }
+        var_dump(self::$process);
+    }
+
+    public function createProcess($i){
+        $process = new Process(function(\swoole_process $process) use($i){
+            echo 'i------>'.$i;
+        });
+        $process->start();
+        return $process;
+    }
+}
+
+$test = new Test();
+$test->index();
+
+
